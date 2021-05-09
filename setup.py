b9fa72c0643600f555a337683ecad4d1af956ab9
shueja-personal <32416547+shueja-personal@users.noreply.github.com>
2021-05-08 20:47:11 -0800

    Remove most requirements that can't be found

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ plugin_url = "https://github.com/AutoprintLabs/Tweaker-3"
 plugin_license = "AGPLv3"
 
 # Any additional requirements besides OctoPrint should be listed here
-plugin_requires = ["numpy", "collections", "re", "zipfile", "os", "sys", "argparse"]
+plugin_requires = ["numpy"]
 
 ### --------------------------------------------------------------------------------------------------------------------
 ### More advanced options that you usually shouldn't have to touch follow after this point
